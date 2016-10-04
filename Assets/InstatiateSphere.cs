7d1cf295cced01afb7ca230ceb0cd7f972807702
mariocova3 <mariocova3@gmail.com>
2016-10-04 01:27:49 -0500

    Got instantiate to work

---
+++ Assets/InstatiateSphere.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class InstatiateSphere : MonoBehaviour
+{
+    private IEnumerator coroutine;
+
+    public GameObject mySphere;
+    GameObject mySphereClone;
+
+    public GameObject myPlayer;
+
+    private Transform playerPosition;
+
+	// Use this for initialization
+	void Start ()
+    {
+        StartCoroutine(InstantiateNow());
+        playerPosition = myPlayer.GetComponent<Transform>() as Transform;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+	
+	}
+
+    IEnumerator InstantiateNow()
+    {
+        yield return new WaitForSeconds(5);
+        mySphere = Instantiate(mySphere, GameObject.Find("FPSController").transform.position, Quaternion.identity) as GameObject;
+    }
+}
